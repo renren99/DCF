a3a848f0b09ab3834bdda2576ff62e4b2c03862e
renmurase <squarex99@gmail.com>
2019-11-02 22:12:28 +0900

    初期

--- README.md
+++
@@ -1,2 +0,0 @@
-# DCF
-DCF法のandroidアプリ
---
+++ app/src/main/java/com/example/dcf/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.dcf.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
